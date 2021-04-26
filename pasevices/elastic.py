93613e7027378826ae37f772bd5993a840a115c5
huangwenqiang <huangwenqiang@chain-c.com.com>
2021-04-26 17:59:26 +0800

    d

---
+++ pasevices/elastic.py
@@ -0,0 +1,21 @@
+import requests
+from services.reques_http import RequestHandler
+from urllib import parse
+import json
+import time
+from services.mysql import MysqlOperation
+
+
+
+
+class Chineseafs:
+    def getElastic(self):
+        print()
+        # for i in range(1,amount):
+        #     print("123456789")
+        #     print(i)
+        #     getOneChineseafst(page=i)
+
+
+def getElastic():
+    print("123456");
