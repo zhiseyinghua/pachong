1834b891b7c8187d63ac9b3fa67183f005c30f69
纸色樱花 <1870132537@qq.com>
2021-05-19 17:04:14 +0800

    d

--- excel/zhuan.py
+++ excel/zhuan.py
@@ -12,13 +12,16 @@ def read_excel():
     print('表格总行数', nrows)
     ncols = sheet1.ncols
     print('表格总列数', ncols)
-    for item in [0, nrows*ncols]:
-        print()
+    for item in [0, nrows]:
+        # print(sheet1.cell(0, 2).value)
+        for item in [0, ncols]:
+            print(sheet1.cell(2, 2).value)
     # row3_values = sheet1.row_values(2)
     # print('第3行值', row3_values)
     # col3_values = sheet1.col_values(2)
     # print('第3列值', col3_values)
     cell_3_3 = sheet1.cell(2, 2).value
+
     print('第3行第3列的单元格的值：', cell_3_3)
 
 
